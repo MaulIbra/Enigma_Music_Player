1a513bc2fcdd021789d0dafb2983b264b78311f6
maulana <maulana@mymindstores.com>
2020-08-21 11:24:39 +0700

    [ADD] logic for play pause audio

--- app/src/main/java/com/example/enigma_music_player/views/song/adapter/SongRecycleAdapter.kt
+++ app/src/main/java/com/example/enigma_music_player/views/song/adapter/SongRecycleAdapter.kt
@@ -11,6 +11,10 @@ import com.example.enigma_music_player.data.room.song.Song
  * Email maulibrahim19@gmail.com
  */
 class SongRecycleAdapter(private val songList: List<Song>):RecyclerView.Adapter<SongViewHolder>() {
+
+    private var isPlayPosition = -1
+    private var isPlay = false
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.song_item,parent,false)
         return SongViewHolder(view)
@@ -23,5 +27,26 @@ class SongRecycleAdapter(private val songList: List<Song>):RecyclerView.Adapter<
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         holder.tvSongTitle.text = songList[position].songTitle
         holder.tvSongDuration.text = songList[position].songDuration
+
+        if (isPlayPosition == position){
+            holder.imgPlayPause.setImageResource(R.drawable.ic_pause_black_24dp)
+        }else{
+            holder.imgPlayPause.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        }
+
+        holder.imgPlayPause.setOnClickListener{
+            if (isPlayPosition == position && isPlay){
+                isPlay = false
+                holder.imgPlayPause.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+            }else if (isPlayPosition == position && !isPlay){
+                isPlay = true
+                holder.imgPlayPause.setImageResource(R.drawable.ic_pause_black_24dp)
+            }else{
+                isPlay = true
+                isPlayPosition = position
+                notifyDataSetChanged()
+            }
+        }
     }
+
 }
